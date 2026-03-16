cc5f92d30a0640f6e7d949efac1409116a1d376a
fcwe1113 <fcwe1113@gmail.com>
2026-03-16 10:42:44 +0000

    comments babyyyyyyyyy

--- Cheuk Wan_Fung.py
+++ Cheuk Wan_Fung.py
@@ -2,3 +2,6 @@
 
 if __name__ == '__main__':
     print(sum(range(1,101)))
+    # range(1, 101) returns a list (or array) of numbers from 1 to 100
+    # sum() returns the added sum of the entered list (or array)
+    # print() well, prints the contents it received
