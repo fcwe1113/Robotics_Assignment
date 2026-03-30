fc5272658fb0f2d28d1cc66bd0088770abb6e337
fcwe1113 <fcwe1113@gmail.com>
2026-03-30 18:33:06 +0100

    implemented namespaced multibot spawning, todo multibot control individually

--- multi_robot/controller.py
+++ multi_robot/controller.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 import roslaunch
+import rospy
+
 from robot_obj import robot_obj
 
 robot_list = {}
@@ -11,14 +13,13 @@ robot_list = {}
 
 ## maybe split robots via subscribe topics
 
-def spawn_bot(x, y) -> robot_obj:
-    name = f"tb3_{str(len(robot_list))}"
+def spawn_bot(name, x, y) -> robot_obj:
     uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
     cli_args = ["rss_assignment", "multi_robot_spawn.launch", f'robot_name:={name}', f"initial_x:=-x {x}", f"initial_y:=-y {y}", "model:=burger"]
     roslaunch_file = [(roslaunch.rlutil.resolve_launch_arguments(cli_args)[0], cli_args[2:])]
     parent = roslaunch.parent.ROSLaunchParent(uuid, roslaunch_file)
     parent.start()
-    return robot_obj(name) # fix this unneeded return
+    return robot_obj(name)
 
 def display_bot_infos() -> str:
     output = ""
@@ -30,7 +31,22 @@ def display_bot_infos() -> str:
     
     return output
 
+def selected_bot_control(selected_bot):
+    while True:
+        inputt = input(f"selected bot:\n{selected_bot.to_string_long()}\n1. update information\n2. move forward\n3. change orientation\n9. exit to main menu")
+        if inputt == "1":
+            pass
+        elif inputt == "2":
+            # do normal movement b4 PID via {not_name}/cmd_vel
+            ...
+        elif inputt == "3":
+            ...
+        elif inputt == "9":
+            print("exitting to main menu...")
+            return
+
 if __name__=="__main__":
+    rospy.init_node(f'multibot_node')
     while True:
         status = f"""number of robots: {len(robot_list)}
         current mode: {11}
@@ -55,17 +71,30 @@ if __name__=="__main__":
                 except ValueError:
                     print("Please enter a valid number(y)")
                 else:
-                    robot_list["tb3_" + str(len(robot_list))] = spawn_bot(x, y)
+                    name = f"tb3_{str(len(robot_list))}"
+                    robot_list[name] = spawn_bot(name, x, y)
                     print(f"new robot spawned at ({x}, {y})")
         elif inputt == "2":
             if len(robot_list) == 0:
                 print("list is empty")
             else:
-                print(str(robot_list))
-                print()
+                for bot in robot_list.items():
+                    print(bot)
 
         elif inputt == "3":
-            print("test")
+            if len(robot_list) == 0:
+                print("list is empty")
+            else:
+                counter = 0
+                for bot in robot_list.items():
+                    print(f"{bot}")
+                    counter += 1
+                
+                inputt = input("Please select robot to control")
+                if inputt < len(robot_list):
+                    selected_bot_control(robot_list[f"tb3_{inputt}"])
+                else:
+                    print("invalid input, returning to main menu")
         elif inputt == "4" or input == '\x03':
             print("exiting...")
             break
--- multi_robot/robot_obj.py
+++ multi_robot/robot_obj.py
@@ -1,5 +1,6 @@
 import numpy as np
 import rospy
+import threading
 
 from typing import Tuple
 from nav_msgs.msg import Odometry
@@ -10,19 +11,49 @@ class robot_obj:
     name: str
     position: Tuple[float, float]
     orientation: float # degrees
+    init_odom_state: bool
+    bot_thread: threading.Thread
+    speed: float # check top speed of bot
+    moving: bool
 
     def __init__(self, name):
         self.name = name
-        rospy.Subscriber(f'{name}/odom', Odometry, self.update) # check if functioning
-        self.movement_control = rospy.Publisher(f'{name}/cmd_vel', Twist, queue_size=10) # check if functioning
+        self.bot_thread = threading.Thread(target=self.spin)
+        self.bot_thread.start()
+        self.init_odom_state = False
     
-    def update(self, msg):
+    def update_callback(self, msg):
+        # print(f"{self.name} callback")
         position = msg.pose.pose.position
+        # print(f"{self.name} pos")
         quat = msg.pose.pose.orientation
+        # print(f"{self.name} quat")
         # line below taken from https://github.com/Tinker-Twins/Autonomy-Science-And-Systems/blob/main/Assignment%203-A/assignment_3a/turtlebot3/turtlebot3/turtlebot3_example/turtlebot3_example/turtlebot3_position_control/turtlebot3_position_control.py
         self.orientation = np.arctan2(2 * (quat.w * quat.z + quat.x * quat.y), 1 - 2 * (quat.y ** 2 + quat.z ** 2))
-        self.position = tuple(position.x, position.y)
+        # print(f"{self.name} angle calc ed")
+        self.position = [position.x, position.y] # tuples assign like mini arrays
+        # print(f"{self.name} pos saved")
+        self.init_odom_state = True
+        # print(f"robot {self.name} callback, pos: {self.position}")
     
     def __repr__(self): # similar to to_string()
-        
-        return f"robot name: {self.name}, position: {self.position}, angle: {self.orientation})"
+
+        return f"robot name: {self.name}, position: {self.position}, angle: {self.orientation}°)"
+    
+    def spin(self):
+        # try:
+        #     rospy.init_node(f'multibot_node', disable_signals=True)
+        # except rospy.exceptions.ROSException as _:
+        #     print("node started already, skipping to bot spawn")
+        rospy.Subscriber(f'{self.name}/odom', Odometry, self.update_callback) # check if functioning
+        self.movement_control = rospy.Publisher(f'{self.name}/cmd_vel', Twist, queue_size=10) # check if functioning
+        rate = rospy.Rate(10)  # 10 Hz meaning it reads data every 100ms
+        # rospy.spin()
+        # print(f"{self.name} robot thread starting")
+        while not rospy.is_shutdown():
+            # do pid stuff here
+            rate.sleep()
+    
+    def to_string_long(self):
+
+        return f"robot name: {self.name}\nposition: {self.position}\nangle: {self.orientation}°)"
